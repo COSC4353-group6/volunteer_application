516e1c51e82311acdf950e6e984dfa345ff497ed
Yeyolive <128828380+Yeyolive@users.noreply.github.com>
2024-11-22 11:32:12 -0600

    added admin and volunteer roles to db

--- server/db.sql
+++ server/db.sql
@@ -18,6 +18,7 @@ CREATE TABLE UserCredentials (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
+  role ENUM('admin', 'volunteer') NOT NULL
 );
 
 CREATE TABLE urgencies (
